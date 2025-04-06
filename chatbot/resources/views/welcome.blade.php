23576ad0e90fbd78e21464fd20d403fff92a6a0e
moisescorado91 <coradomoises91@gmail.com>
2025-04-06 13:28:21 -0600

    sugerencias de interface chatbot

--- chatbot/resources/views/welcome.blade.php
+++ chatbot/resources/views/welcome.blade.php
@@ -1,16 +1,23 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <link rel="icon" type="image/svg+xml" href="https://chatboot-production-9c1c.up.railway.app/assets/assets/chatbot.png" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>HipertensoBot</title>
-    <script type="module" crossorigin src="http://127.0.0.1:8000/index-DUdpaUd0.js"></script>
-    <link rel="stylesheet" crossorigin href="http://127.0.0.1:8000/index-DmkIT-Ye.css">
-  </head>
-  <body>
-    <main id="root">
-    </main>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  </body>
+
+<head>
+  <meta charset="UTF-8" />
+  <link rel="icon" type="image/svg+xml" href="http://127.0.0.1:8000/public.avif" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+  <title>Cardio Salud Es</title>
+
+  <script type="module" crossorigin src="http://127.0.0.1:8000/index-Gu8NX0fY.js"></script>
+  <link rel="stylesheet" crossorigin href="http://127.0.0.1:8000/index-BL2q7XJb.css">
+</head>
+
+<body>
+  <main id="root">
+  </main>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+    integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+</body>
+
 </html>
